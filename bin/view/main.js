656038977a7e139761e74e2c8f07b99f5c427033
Hiromitsu Nakajima <office@lichenmoss.net>
2020-05-09 15:04:20 +0900

    js keywords

--- bin/view/main.js
+++ bin/view/main.js
@@ -1,11 +1,12 @@
 window.onload = function () {
     lastArticles(10);
+    linkKeywords();
 }
 
 function lastArticles(num){
     var httpReq = new XMLHttpRequest();
     httpReq.onreadystatechange = function(){
-        if(httpReq.readyState !=4 || httpReq.status != 200)
+        if(httpReq.readyState != 4 || httpReq.status != 200)
 	    return;
 	
 	document.getElementById("last-articles").innerHTML = httpReq.responseText;
@@ -14,3 +15,18 @@ function lastArticles(num){
     httpReq.open("GET",url,true);
     httpReq.send(null);
 }
+
+function linkKeywords(){
+    var httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(httpReq.readyState != 4 || httpReq.status != 200)
+            return;
+
+        document.getElementById("keywords").innerHTML = httpReq.responseText;
+    }
+    var word = document.getElementById("keywords").innerHTML;
+    var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
+    httpReq.open("GET",url,true);
+    httpReq.send(null);
+}
+
